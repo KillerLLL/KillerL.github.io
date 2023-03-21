2e9ab3b02dca6e25dd311b17a2585d3e9854b885
KillerLLL <104113683+KillerLLL@users.noreply.github.com>
2023-03-21 14:01:53 +0800

    add

--- baseUrl.js
+++
@@ -1,3 +0,0 @@
-// window.baseUrl = 'http://192.168.70.42:8948/boot'
-window.baseUrl = 'https://mock.mengxuegu.com/mock/63dc7cd81b8291742151c4a3/linmock'
-window.baseUrl2 = 'http://192.168.0.188:8000'
