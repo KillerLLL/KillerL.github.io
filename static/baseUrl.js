d9c4423cdd8c086957c7d188cbd20b6bc8bfce74
KillerLLL <104113683+KillerLLL@users.noreply.github.com>
2023-03-23 13:52:38 +0800

    add

--- static/baseUrl.js
+++ static/baseUrl.js
@@ -1,3 +1,15 @@
 // window.baseUrl = 'http://192.168.70.42:8948/boot'
 window.baseUrl = 'https://mock.mengxuegu.com/mock/63dc7cd81b8291742151c4a3/linmock'
 window.baseUrl2 = 'http://192.168.0.188:8000'
+
+window.photo = [
+  {
+    content:'static/image/xpq5.jpg'
+  },
+  {
+    content:'static/image/xpq6.jpg'
+  },
+  {
+    content:'static/image/xpq4.jpg'
+  },
+]
